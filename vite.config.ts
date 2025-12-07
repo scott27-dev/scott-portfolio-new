21318f26b419708528d092820b233b0b897bdcfa
scottkinchlea <scotta.kinchlea@gmail.com>
2025-12-07 18:17:00 +0000

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repoName = 'scott-portfolio-new'; 
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
+  base: `/`,
 
   build: {
     outDir: 'dist',
